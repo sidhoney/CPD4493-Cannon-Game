1b7165dea2201f466bfd1ca3a4545b9edbbc14a4
Sidhartha <sidharthagopinath@outlook.com>
2015-02-27 08:18:19 -0500

    Extened App

---
+++ src/com/deitel/cannongame/Line.java
@@ -0,0 +1,11 @@
+// Line.java
+// Class Line represents a line with two end points.
+package com.deitel.cannongame;
+
+import android.graphics.Point;
+
+public class Line {
+
+    public Point start = new Point(); // start Point--(0,0) by default
+    public Point end = new Point(); // end Point--(0,0) by default
+} // end class Line
---
+++ src/com/deitel/cannongame/MainActivity.java
@@ -0,0 +1,17 @@
+// CannonGame.java
+// MainActivity displays the CannonGameFragment
+package com.deitel.cannongame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    // called when the app first launches
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // call super's onCreate method
+        setContentView(R.layout.activity_main); // inflate the layout
+    }
+} // end class MainActivity
